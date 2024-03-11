cde64792098252f220f34d8e1f77daa5d0e67ef0
Luis Xool <langelxobl@gmail.com>
2024-03-11 18:24:31 -0500

    First commit

---
+++ src/middleware/index.ts
@@ -0,0 +1,17 @@
+import { defineMiddleware } from "astro:middleware";
+
+const unprotectedRoutes = ["/", "/login"];
+
+const middleware = defineMiddleware(({ url, redirect }, next) => {
+  console.log(
+    "Ejecutando middleware antes del renderizado",
+    `Route: ${url.pathname}`
+  );
+  if (unprotectedRoutes.includes(url.pathname)) {
+    return next();
+  }
+  console.log("Redirect -> login");
+  return redirect("/login");
+});
+
+export const onRequest = middleware;
